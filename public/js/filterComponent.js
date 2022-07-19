0891c57fd2d1e91df63bd7c766a52f498d565202
hooope81 <hooope81@gmail.com>
2022-07-19 23:35:25 +0300

    node.js1

--- public/js/filterComponent.js
+++ public/js/filterComponent.js
@@ -9,7 +9,7 @@ const filter_el = {
                     <form action="#" @submit.prevent="$parent.$refs.product.filter(userSearch)">
                             <input type="text" class="filter__input" v-model="userSearch">
                             <button type="submit">
-                            <img src="../../img/search.png" alt="cart">
+                            <img src="../img/search.png" alt="cart">
                         </button>
                     </form>
                 </div>
